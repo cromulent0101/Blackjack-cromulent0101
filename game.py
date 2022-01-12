d88e63e52518d9c15d3b59546c1f34722b4ddbf5
Joe Kaiser <kaiserfjoe@gmail.com>
2022-01-11 21:03:31 -0500

    Move dealing to a function

--- game.py
+++ game.py
@@ -24,33 +24,33 @@ def getHandValue(hand): # easiest to determine value of cards in hand context be
 def checkBlackjack():
     pass
 
+def dealCard(deck):
+    return (deck.pop() % 13)+1
+
 deck = ([i for i in range(0,51)]) 
 random.shuffle(deck) # deck should be shuffled and we sequentially remove cards from list using pop
 print(deck)
 # players place bets
  
 startingAmount = 100
-bet = int(input("Enter your bet: "))
+# bet = int(input("Enter your bet: "))
 
 # dealer deals first round
 
-dFaceDown = deck.pop # hidden from players/dealer but dFaceUp is an ace, dealer checks
-# we will need to convert face cards into their true value once added to hand
-## but what about spliting?
-# aces may be con
-p1 = (deck.pop() % 13)+1 # cards represnted as [1,13] but these are not their values
-print(p1)
-p2 = (deck.pop() % 13)+1
+p1 = dealCard(deck) # cards represnted as [1,13] but these are not their values
+d1 = dealCard(deck) # hidden from players/dealer but dFaceUp is an ace, dealer checks
+p2 = dealCard(deck)
+d2 = dealCard(deck) # visible to player
 
 # create player's hand
 pHand=[p1,p2] # list of two integers
 print(p1," ",p2)
+print(getHandValue(pHand))
 # if player gets blackjack they instantly win 1.5 value of their bet
-if pHand.count(1) and (pHand.count(10) or pHand.count(11) or pHand.count(12) or pHand.count(13)))
 
 # if dealer gets blackjack they instantly win
 
-print(bet)
-print(bet*30)
+# print(bet)
+# print(bet*30)
 
 # player chooses to hit, stand, double down, or split
