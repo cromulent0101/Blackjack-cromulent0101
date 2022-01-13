c22a1cd971c4aa189323441e0764b0e5f8dc9ce4
Joe Kaiser <kaiserfjoe@gmail.com>
2022-01-12 21:00:08 -0500

    Add hit/stand decision and check blackjacks

--- game.py
+++ game.py
@@ -2,12 +2,19 @@ import os
 import datetime
 import random
 
+cardMap = {1:"Ace", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine", 10:"Ten", 11:"Jack", 12:"Queen", 13:"King"}
+
+def printPlayerHand(hand): # print player's hand cards
+    # print("Player hand: ")
+    #     for card in hand: print(cardMap[card])
+    # print(cardMap[hand])
+
+
 def convertNonAceCard(card): # card will be [2,13]
     if card > 10:
         return 10
     else:
         return card 
-    
 
 def getHandValue(hand): # easiest to determine value of cards in hand context because of aces
     total = 0
@@ -30,27 +37,46 @@ def dealCard(deck):
 deck = ([i for i in range(0,51)]) 
 random.shuffle(deck) # deck should be shuffled and we sequentially remove cards from list using pop
 print(deck)
-# players place bets
  
-startingAmount = 100
+chips = 100
 # bet = int(input("Enter your bet: "))
-
-# dealer deals first round
+bet = 10
 
 p1 = dealCard(deck) # cards represnted as [1,13] but these are not their values
 d1 = dealCard(deck) # hidden from players/dealer but dFaceUp is an ace, dealer checks
 p2 = dealCard(deck)
 d2 = dealCard(deck) # visible to player
 
+printPlayerHand([1,2,3,4])
+
 # create player's hand
 pHand=[p1,p2] # list of two integers
-print(p1," ",p2)
-print(getHandValue(pHand))
+dHand=[d1,d2]
 # if player gets blackjack they instantly win 1.5 value of their bet
-
-# if dealer gets blackjack they instantly win
-
-# print(bet)
-# print(bet*30)
-
-# player chooses to hit, stand, double down, or split
+print(f"Dealer card:   {cardMap[d2]}")
+# print(f"Player's hand value: {getHandValue(pHand)} ") 
+if getHandValue(pHand)==21 and getHandValue(dHand)==21:
+    # player pushes
+    print("Player cards: ",cardMap[p1]," ",cardMap[p2])
+    print(f"Push!")
+    # break
+elif getHandValue(dHand)==21:
+    # dealer blackjack
+    print("Player cards: ",cardMap[p1]," ",cardMap[p2])
+    print("Dealer blackjack!")
+    chips -= bet
+    # break
+elif getHandValue(pHand)==21:
+    # player blackjack
+    print("Player cards: ",cardMap[p1]," ",cardMap[p2])
+    print("Player blackjack!")
+    chips += bet*(3/2)
+    # break
+else: # player chooses to hit, stand, double down, or split
+    while getHandValue(pHand) < 21: # only give player options when value below 21
+        print("Player cards: ",cardMap[p1]," ",cardMap[p2])
+        action = input("Hit or stand? ").lower()
+        if action == "hit":
+            print("foo")
+            pHand.append(dealCard(deck))
+            break
