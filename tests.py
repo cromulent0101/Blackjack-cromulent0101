18c077b15a7a060150c7c88b45c22d572df6944b
Joe Kaiser <kaiserfjoe@gmail.com>
2022-01-20 07:41:56 -0500

    Revert "Add blackjack unit test"
    This reverts commit 3234d3a248807bdb511adda53335965394f1b051.

--- tests.py
+++ tests.py
@@ -14,14 +14,5 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(getHandValue([10,7])[1],False)
         self.assertEqual(getHandValue([1,1,1,1,1,2])[1],True)
 
-    def test_alternating_aces(self):
-        self.assertEqual(getHandValue([1,6])[0],17)
-        self.assertEqual(getHandValue([1,6,1])[0],18)
-        self.assertEqual(getHandValue([1,6,1,4])[0],12)
-
-    def test_crazy_blackjack(self):
-        self.assertEqual(getHandValue([2,2,2,2,2,2,2,2,2,2,1])[0],21)
-
-
 if __name__ == '__main__':
     unittest.main()
