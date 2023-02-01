b0f2222d96a92c1aafc6beae8b8eaaae2599e37d
Joe Kaiser <kaiserfjoe@gmail.com>
2023-01-31 22:52:18 -0500

    Improve documentation and readability

--- game.py
+++ game.py
@@ -3,51 +3,56 @@ import time
 from cardutils import getHandValue,printDealerHand,convertNonAceCard,cardMap,Hand,dealCard,convertNonAceCard
 from strategy import split_strat, strat
 
-debug: False
+DEBUG = False
 
-## settings ##
-ddas = True
-s17 = False     # Does dealer stand on soft 17?
-surrender = False
+''' settings '''
+ddas = True             # Can you double down after a split?
+s17 = False             # Does dealer stand on soft 17?
+surrender = False       # TODO: implement surrender
 onlySplitPairs = False
 decks = 6
 chips = 100    
 hands = []
 
 
-def playHand(hand: Hand, split: bool, j: int): 
-    if hand.s: hand.dealCard(deck)          # if this hand is a product of split, deal another card
-    if False: # debug
+def playHand(hand: Hand, split: bool, j: int) -> None:
+    ''' 
+    Main loop for the sim.
+
+    Given a Hand, play Blackjack until you win, lose, or push.
+    '''
+    if hand.split: hand.dealCard(deck)          # if this hand is a product of split, deal another card
+    if DEBUG: # debug
         hand.printPlayerHand()
         print(f"this is the {j} time playing a hand")
-        print(f"This hand is a split: {hand.s}")
-    j += 1
+        print(f"This hand is a split: {hand.split}")
+        j += 1
     # global hands # don't know if we need this
     global chips
     if hand.value == 21: 
         hand.printPlayerHand()
         hands.append(hand)
         print("Player blackjack!")
-        chips += round(hand.b*(5/2))
+        chips += round(hand.bet*(5/2))
     while hand.value < 21:              # play blackjack! only give player options when value below 21
         hand.printPlayerHand()
         time.sleep(1)
         action = input("Hit, stand, double, or split? ").lower()
         # get the correct action for this hand
-        if(len(hand.c)==2):
+        if(len(hand.cards)==2):
             if (onlySplitPairs):
-                if(hand.c[0] == hand.c[1]):
+                if(hand.cards[0] == hand.cards[1]):
                     correct_action = split_strat[(convertNonAceCard(hand.c[0]),convertNonAceCard(faceUpCard))]
                 else:
                     correct_action = strat[(hand.value,hand.soft,convertNonAceCard(faceUpCard))]
             else:
-                if convertNonAceCard(hand.c[0]) == convertNonAceCard(hand.c[1]):
-                    correct_action = split_strat[(convertNonAceCard(hand.c[0]),convertNonAceCard(faceUpCard))]
+                if convertNonAceCard(hand.cards[0]) == convertNonAceCard(hand.cards[1]):
+                    correct_action = split_strat[(convertNonAceCard(hand.cards[0]),convertNonAceCard(faceUpCard))]
                 else:
                     correct_action = strat[(hand.value,hand.soft,convertNonAceCard(faceUpCard))]
         else:
             correct_action = strat[(hand.value,hand.soft,convertNonAceCard(faceUpCard))]
-            if (correct_action == '\x1b[0;31;40m'+'Double'+'\x1b[0m' and len(hand.c)>2):
+            if (correct_action == '\x1b[0;31;40m'+'Double'+'\x1b[0m' and len(hand.cards)>2):
                 correct_action = '\x1b[0;33;40m'+'Hit'+'\x1b[0m'
 
         if action.startswith("h"):
@@ -68,9 +73,9 @@ def playHand(hand: Hand, split: bool, j: int):
                 print("Correct choice!")
             else:
                 print(f"Wrong, you should {correct_action}")
-            if ((len(hand.c) == 2) and (ddas or not hand.s)):  # only allow doubles on two cards
-                chips -= hand.b
-                hand.b += hand.b
+            if ((len(hand.cards) == 2) and (ddas or not hand.split)):  # only allow doubles on two cards
+                chips -= hand.bet
+                hand.bet += hand.bet
                 hand.dealCard(deck)
                 if hand.value > 21:
                     hand.printPlayerHand()
@@ -78,7 +83,7 @@ def playHand(hand: Hand, split: bool, j: int):
                 if hand.value == 21:
                     hand.printPlayerHand()
                     print("Player blackjack!")
-                    chips += round(hand.b*(5/2))
+                    chips += round(hand.bet*(5/2))
                     time.sleep(1)
                 hands.append(hand)
                 break
@@ -89,20 +94,20 @@ def playHand(hand: Hand, split: bool, j: int):
             else:
                 print(f"Wrong, you should {correct_action}")
             if onlySplitPairs:
-                if hand.c[0] != hand.c[1]:
+                if hand.cards[0] != hand.cards[1]:
                     print("You can't split non-pairs") # at some casinos you can split face cards
                 else:
-                    playHand(Hand([pHand.c[0]],hand.b,True),True,j)
-                    chips -= hand.b
-                    playHand(Hand([pHand.c[1]],hand.b,True),True,j)
+                    playHand(Hand([pHand.cards[0]],hand.bet,True),True,j)
+                    chips -= hand.bet
+                    playHand(Hand([pHand.cards[1]],hand.bet,True),True,j)
                     break
             else:
-                if convertNonAceCard(hand.c[0]) != convertNonAceCard(hand.c[1]):
+                if convertNonAceCard(hand.cards[0]) != convertNonAceCard(hand.cards[1]):
                     print("You can't split cards of different values") # at some casinos you can split face cards
                 else:
-                    playHand(Hand([pHand.c[0]],hand.b,True),True,j)
-                    chips -= hand.b
-                    playHand(Hand([pHand.c[1]],hand.b,True),True,j)
+                    playHand(Hand([pHand.cards[0]],hand.bet,True),True,j)
+                    chips -= hand.bet
+                    playHand(Hand([pHand.cards[1]],hand.bet,True),True,j)
                     break
         else:
             print("Not a valid move")
@@ -114,7 +119,7 @@ def playHand(hand: Hand, split: bool, j: int):
             hand.printPlayerHand()
             hands.append(hand)
             print("Player blackjack!")
-            chips += round(hand.b*(5/2))
+            chips += round(hand.bet*(5/2))
             time.sleep(1)
 
 
@@ -123,19 +128,19 @@ for i in range(decks):
     deck.extend([i for i in range(0,51)])               # will add ability for multi-deck
 random.shuffle(deck) # deck should be shuffled and we sequentially remove cards from list using pop
 
-# deck = [5,6]  # dealyer's cards
-# # deck.extend([3,3,4,3,5,3,3,3,8,9,7,11,12,13,3,3,4]) # players cards
-# deck.extend([3,2,3,5,5,5]) # players cards
 
-while(chips > 0):
+while (chips > 0):
     while True:
         try:
             bet = int(input("Enter your bet: "))
             break
         except ValueError:
-            print("Invalid bet")
-    if bet < 1 or bet > chips:
-        print("Invalid bet")
+            print("Bet must be an integer.")
+    if bet < 1:
+        print("Bet must be positive.")
+        continue
+    elif bet > chips:
+        print(f"Bet must be less than or equal to the {chips} chips you currently have.")
         continue
 
     holeCard = dealCard(deck) # hidden from players/dealer but if hole card is an ace, dealer checks
@@ -180,26 +185,24 @@ while(chips > 0):
                 playable_hands.append(hand)
         if playable_hands:
             printDealerHand(dHand)
-        # i = 1
+
         for hand in playable_hands:
             hand.printPlayerHand()
-            # print(f"this is hand {i}")
-            i += 1
             if hand.value > 21: 
                 print(f"Player busts! ({hand.value})")
             else:
                 if getHandValue(dHand)[0] > 21:
                     print(f"Dealer busts! ({getHandValue(dHand)[0]})")
-                    chips += hand.b*2
+                    chips += hand.bet*2
                     continue
                 if hand.value > getHandValue(dHand)[0]:
                     print(f"Player ({hand.value}) beats dealer ({getHandValue(dHand)[0]})!")
-                    chips += hand.b*2
+                    chips += hand.bet*2
                 elif (hand.value < getHandValue(dHand)[0]) and (getHandValue(dHand)[0] < 22): # dealer can still get BJ here
                     print(f"Dealer ({getHandValue(dHand)[0]}) beats player ({hand.value})!")
                 else:
                     print(f"Push! ({getHandValue(dHand)[0]})")
-                    chips += hand.b
+                    chips += hand.bet
     hands = []
     print("Chips: ",chips)
     
